a1398e9f0653cf8564dafc93a4246c35eb2b0fcc
Dmitriy Ryabov <dima.poltergeist.2015.@yandex.ru>
2022-09-21 23:49:55 +0300

    6

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,89 +1,89 @@
-class Api  //название файла так и стоит, как вы просили, api.js, а название класса если меняю, выдает ошибку
+class Api  //название файла так и стоит, как вы просили, api.js, а название класса если меняю, выдает ошибку, в vs code у меня стоит с маленькой, а на гитхаб скидываю почему то не меняется((
+    {
+    constructor({ baseUrl, headers })
 {
-    constructor ({ baseUrl, headers })
-    {
-        this._headers = headers;
-        this._baseUrl = baseUrl;
-    }
+    this._headers = headers;
+    this._baseUrl = baseUrl;
+}
 
-    //проверка ответа от сервера
-    _checkResponse(res)
-    {
-        if (res.ok) {
-            return res.json();
-        }
-        return Promise.reject(res.status);
+//проверка ответа от сервера
+_checkResponse(res)
+{
+    if (res.ok) {
+        return res.json();
     }
+    return Promise.reject(res.status);
+}
 
-    //получение данных профиля
-    getProfile()
-    {
-        return fetch(`${this._baseUrl}/users/me`, {
-            headers: this._headers,
-        }).then(this._checkResponse);
-    }
+//получение данных профиля
+getProfile()
+{
+    return fetch(`${this._baseUrl}/users/me`, {
+        headers: this._headers,
+    }).then(this._checkResponse);
+}
 
-    //получение карточек
-    getInitialCards()
-    {
-        return fetch(`${this._baseUrl}/cards`, {
-            headers: this._headers,
-        }).then(this._checkResponse);
-    }
+//получение карточек
+getInitialCards()
+{
+    return fetch(`${this._baseUrl}/cards`, {
+        headers: this._headers,
+    }).then(this._checkResponse);
+}
 
-    //редактирование профиля
-    editProfile(name, about)
-    {
-        return fetch(`${this._baseUrl}/users/me`, {
-            method: "PATCH",
-            headers: this._headers,
-            body: JSON.stringify({
-                name,
-                about,
-            }),
-        }).then(this._checkResponse);
-    }
+//редактирование профиля
+editProfile(name, about)
+{
+    return fetch(`${this._baseUrl}/users/me`, {
+        method: "PATCH",
+        headers: this._headers,
+        body: JSON.stringify({
+            name,
+            about,
+        }),
+    }).then(this._checkResponse);
+}
 
-    //добавление новых карточек
-    addNewCard(name, link)
-    {
-        return fetch(`${this._baseUrl}/cards`, {
-            method: "POST",
-            headers: this._headers,
-            body: JSON.stringify({
-                name,
-                link,
-            }),
-        }).then(this._checkResponse);
-    }
+//добавление новых карточек
+addNewCard(name, link)
+{
+    return fetch(`${this._baseUrl}/cards`, {
+        method: "POST",
+        headers: this._headers,
+        body: JSON.stringify({
+            name,
+            link,
+        }),
+    }).then(this._checkResponse);
+}
 
-    //удаление карточек
-    deleteCard(id)
-    {
-        return fetch(`${this._baseUrl}/cards/${id}`, {
-            method: "DELETE",
-            headers: this._headers,
-        }).then(this._checkResponse);
-    }
+//удаление карточек
+deleteCard(id)
+{
+    return fetch(`${this._baseUrl}/cards/${id}`, {
+        method: "DELETE",
+        headers: this._headers,
+    }).then(this._checkResponse);
+}
 
-    //удаление и постановка лайков
-    changeLikeCardStatus(id, likeStatus)
-    {
-        return fetch(`${this._baseUrl}/cards/${id}/likes`, {
-            method: likeStatus ? "PUT" : "DELETE",
-            headers: this._headers,
-        }).then(this._checkResponse);
-    }
+//удаление и постановка лайков
+changeLikeCardStatus(id, likeStatus)
+{
+    return fetch(`${this._baseUrl}/cards/${id}/likes`, {
+        method: likeStatus ? "PUT" : "DELETE",
+        headers: this._headers,
+    }).then(this._checkResponse);
+}
 
-    //редактирование аватара
-    changeAvatar(link)
-    {
-        return fetch(`${this._baseUrl}/users/me/avatar`, {
-            method: "PATCH",
-            headers: this._headers,
-            body: JSON.stringify({ avatar: link }),
-        }).then(this._checkResponse);
-    }
+//редактирование аватара
+changeAvatar(link)
+{
+    return fetch(`${this._baseUrl}/users/me/avatar`, {
+        method: "PATCH",
+        headers: this._headers,
+        body: JSON.stringify({ avatar: link }),
+    }).then(this._checkResponse);
+}
 }
 
 
